e05300a0df13d40a50d24209192401182aee6a5d
X.Heart <xiaoxin.wang@xinhuajindian.com>
2018-04-04 10:14:39 +0800

    a

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: X_Heart
  * @Date: 2017-06-07 09:08:00
  * @Last Modified by: wangxiaoxin
- * @Last Modified time: 2018-04-04 09:30:51
+ * @Last Modified time: 2018-04-04 10:14:03
  * @description: 班级说说
  */
 
@@ -22,22 +22,6 @@ app.set('port', (process.env.PORT || 5000));
 // 静态文件中间件
 app.use(express.static('./public'))
 app.use('/avatar', express.static('./avatar'))
-// session配置
-// app.use(session({
-//     secret: 'keyboard cat',
-//     resave: false,
-//     saveUninitialized: true
-// }))
-app.use(session({
-  name: identityKey,
-  secret: 'chyingp',  // 用来对session id相关的cookie进行签名
-  store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
-  saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
-  resave: false,  // 是否每次都重新保存会话，建议false
-  cookie: {
-      maxAge: 10 * 1000  // 有效期，单位是毫秒
-  }
-}));
 // 解决favicon.ico请求问题
 app.use((req, res, next) => {
   if(req.url == "/favicon.ico") {
@@ -46,6 +30,17 @@ app.use((req, res, next) => {
     next()
   }
 })
+// session配置
+app.use(session({
+  name: identityKey,
+  secret: 'keyboard',  // 用来对session id相关的cookie进行签名
+  store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+  saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
+  resave: false,  // 是否每次都重新保存会话，建议false
+  cookie: {
+      maxAge: 10 * 1000  // 有效期，单位是毫秒
+  }
+}));
 app.locals.dateFormat = function(date){
     return sd.format(date, 'YYYY-MM-DD HH:mm')
 }
